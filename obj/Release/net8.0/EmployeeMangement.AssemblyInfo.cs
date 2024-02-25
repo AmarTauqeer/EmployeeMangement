dc6f9b9dddbf1c80c69b6eefd4881267184727e3
Amar Tauqeer <amar.tauqeer@gmail.com>
2024-02-25 10:18:41 +0100

    remove all the deployments

--- obj/Release/net8.0/EmployeeMangement.AssemblyInfo.cs
+++ obj/Release/net8.0/EmployeeMangement.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeMangement")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85b05c44a4640f94f8d897150cc35b79de33f586")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeMangement")]
